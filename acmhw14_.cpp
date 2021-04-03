2839039ecdb37afc70d0b7f91f6769309f5d0485
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2021-04-03 19:25:55 +0300

    Update and rename acmhw14.cpp to acmhw14_.cpp

--- acmhw14_.cpp
+++ acmhw14_.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 /*************************************************************************
 * N.Kozak // Lviv'2020 // ACM // BigValue implementation and use example *
-*    file: acmhw14.cpp                                                   *
+*    file: acmhw14_.cpp                                                  *
 **************************************************************************/
 #include "stdio.h"
 #include "stdlib.h"
